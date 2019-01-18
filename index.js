f7c80b1d0464a067cb01953fb6f231969a28370e
loveencounterflow <paragate@gmx.net>
2019-01-18 20:16:30 +0100

    cleanup code

--- index.js
+++ index.js
@@ -14,56 +14,64 @@ module.exports = function (writer, ender) {
     }
 
     ender = ender || function () {
-      this.queue(null)
+      this.queue( end_sym )
     }
 
     var emitter = {
-      emit: function (event, data) {
-        if(event == 'data') enqueue(data)
-        if(event == 'end')  ended = true, enqueue(null)
-        if(event == 'error') error = data
+      emit: function ( event, data ) {
+        if ( event === 'data'   ) { enqueue(data); };
+        if ( event === 'end'    ) { ended = true, enqueue( end_sym ); };
+        if ( event === 'error'  ) { error = data; };
       },
       queue: enqueue
     }
     var _cb
     return function (end, cb) {
       ended = ended || end
-      if(end)
+      if ( end ) {
         return read(end, function () {
           if(_cb) {
             var t = _cb; _cb = null; t(end)
           }
           cb(end)
-        })
+        }) };
 
       _cb = cb
-      looper(function pull (next) {
+      const pull = function ( next ) {
         //if it's an error
-        if(!_cb) return
-        cb = _cb
-        if(error) _cb = null, cb(error)
-        else if(queue.length) {
-          var data = queue.shift()
-          _cb = null,cb(data === null, data)
-        }
+        if ( !_cb ) { return; };
+        cb = _cb;
+        if ( error ) {
+          _cb = null;
+          cb( error ); }
+        else if ( queue.length > 0 ) {
+          var data  = queue.shift();
+          _cb       = null;
+          cb( data === end_sym, data );
+          }
         else {
-          read(ended, function (end, data) {
+          // ...............................................................................................
+          read( ended, function ( end, data ) {
              //null has no special meaning for pull-stream
-            if(end && end !== true) {
-              error = end; return next()
-            }
-            if(ended = ended || end)  ender.call(emitter)
-            else if(data !== end_sym) {
-              writer.call(emitter, data)
-              if(error || ended)
-                return read(error || ended, function () {
-                  _cb = null; cb(error || ended)
-                })
-            }
-            next(pull)
+            if( end && end !== true ) {
+              error = end;
+              return next(); };
+            // .............................................................................................
+            if( ended = ended || end ) {
+              ender.call( emitter ); }
+            else if( data !== end_sym ) {
+              writer.call( emitter, data );
+              if ( error || ended ) {
+                return read( error || ended, function () {
+                  _cb = null; cb( error || ended )
+                }) };
+              };
+            next( pull );
           })
         }
-      })
+      };
+
+      looper( pull );
     }
   }
 }
